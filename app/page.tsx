66d7b29ca686da93362938c3ef2e67caec430754
Matheus Dantas <matheus.kohaku.dantas@gmail.com>
2024-02-19 23:48:41 -0300

    finished experience section

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </nav>
       <div className="hero">
         <div className="title mt-20 w-100 flex justify-center">
-          <h1 className="text-6xl text-center font-bold max-w-3xl mt-20 leading-tight">
+          <h1 className="text-6xl sm:max-md:text-4xl text-center font-bold max-w-3xl mt-20 leading-tight">
             Experienced freelance developer creating innovative solutions
           </h1>
         </div>
@@ -28,7 +28,27 @@ export default function Home() {
           </button>
         </div>
 
-          <Carousel />
+        <Carousel />
+
+        <div className="experience flex sm:max-md:flex-col mt-40 px-20">
+          <div className="experience-info w-2/4">
+            <p className="font-bold">Experienced</p>
+            <h1 className="title text-5xl  sm:max-md:text-4xl font-bold max-w-3xl mt-5 leading-tight w-3/4">Delivering Exceptional Results with Personalized Service</h1>
+            <p className="w-5/6">As a freelance developer, I bring expertise, personalized service, and flexibility to every project. With years of experience, I have a deep understanding of various technologies and can deliver exceptional results tailored to your specific needs.</p>
+            <ul className="list-image-[url(/check.png)] p-10 list-outside text-lg">
+              <li>Expertise in Cutting-Edge Technologies</li>
+              <li>Custom Solutions for Your Business</li>
+              <li>Flexible and Agile Development Process</li>
+            </ul>
+
+            <button className="bg-white  border-blue-400 border-1 py-3 mt-5 mr-20 rounded-lg hover:text-blue-400 transition-all delay-75">
+              Learn more >
+            </button>
+          </div>
+          <div className="image w-2/4 flex justify-center">
+            <Image className="object-cover shrink-0" src="/experienced.jpg" alt="A photo representing personalized service" height={600} width={800} />
+          </div>
+        </div>
       </div>
     </div>
   );
